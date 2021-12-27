3eb61e4084d845e80806c57eb066a23dd8193a59
Oha-you <Oha-you@users.noreply.github.com>
2021-12-27 22:30:27 +0300

    Don't convert images, use correct mime type to allow GIFs

--- sticker/lib/util.py
+++ sticker/lib/util.py
@@ -25,9 +25,7 @@ from . import matrix
 open_utf8 = partial(open, encoding='UTF-8')
 
 def convert_image(data: bytes) -> (bytes, int, int):
-    image: Image.Image = Image.open(BytesIO(data)).convert("RGBA")
-    new_file = BytesIO()
-    image.save(new_file, "png")
+    image: Image.Image = Image.open(BytesIO(data))
     w, h = image.size
     if w > 256 or h > 256:
         # Set the width and height to lower values so clients wouldn't show them as huge images
@@ -37,7 +35,7 @@ def convert_image(data: bytes) -> (bytes, int, int):
         else:
             w = int(w / (h / 256))
             h = 256
-    return new_file.getvalue(), w, h
+    return w, h
 
 
 def add_to_index(name: str, output_dir: str) -> None:
@@ -56,7 +54,7 @@ def add_to_index(name: str, output_dir: str) -> None:
         print(f"Added {name} to {index_path}")
 
 
-def make_sticker(mxc: str, width: int, height: int, size: int,
+def make_sticker(mxc: str, width: int, height: int, size: int, mime_type: str = "image/png",
                  body: str = "") -> matrix.StickerInfo:
     return {
         "body": body,
@@ -65,7 +63,7 @@ def make_sticker(mxc: str, width: int, height: int, size: int,
             "w": width,
             "h": height,
             "size": size,
-            "mimetype": "image/png",
+            "mimetype": mime_type,
 
             # Element iOS compatibility hack
             "thumbnail_url": mxc,
--- sticker/pack.py
+++ sticker/pack.py
@@ -54,6 +54,9 @@ async def upload_sticker(file: str, directory: str, old_stickers: Dict[str, matr
     if not mime.startswith("image/"):
         return None
 
+    # Uncomment below to force "image/gif" mime type for APNGs or animated WEBPs
+    # mime = "image/gif"
+
     print(f"Processing {file}", end="", flush=True)
     try:
         with open(path, "rb") as image_file:
@@ -77,11 +80,11 @@ async def upload_sticker(file: str, directory: str, old_stickers: Dict[str, matr
         }
         print(f".. using existing upload")
     else:
-        image_data, width, height = util.convert_image(image_data)
+        width, height = util.convert_image(image_data)
         print(".", end="", flush=True)
-        mxc = await matrix.upload(image_data, "image/png", file)
+        mxc = await matrix.upload(image_data, mime, file)
         print(".", end="", flush=True)
-        sticker = util.make_sticker(mxc, width, height, len(image_data), name)
+        sticker = util.make_sticker(mxc, width, height, len(image_data), mime, name)
         sticker["id"] = sticker_id
         print(" uploaded", flush=True)
     return sticker
